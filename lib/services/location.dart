3f819a5c335a9a740ad72c27a7ba971ae98c2135
leo lam <raft-a-hill@leodeMac-mini.local>
2023-11-07 17:37:38 +0800

    clima app_macmini_version

---
+++ lib/services/location.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double? longitude;
+  double? latitude;
+
+  Future<void> getCurrentLocation() async {
+    try {
+      LocationPermission permission = await Geolocator.requestPermission();
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low);
+      latitude = position.latitude;
+      longitude = position.longitude;
+    } catch (e) {
+      print(e);
+    }
+  }
+}
